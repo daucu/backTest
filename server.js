b6400d93d8ea8d998f84b28c17295ea3c6c07112
Deniscartin <100749943+Deniscartin@users.noreply.github.com>
2024-08-31 02:06:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors(corsOptions));
 
 app.get('/api/customers', (req, res) => {
   const customers = [
-    { id: 1, firstName: 'John', lastName: 'Doe' },
+    { id: 1, firstName: 'Pietro', lastName: 'Doe' },
     { id: 2, firstName: 'Brad', lastName: 'Traversy' },
     { id: 3, firstName: 'Mary', lastName: 'Swanson' },
   ];
